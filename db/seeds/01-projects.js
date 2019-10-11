fdb775b3458a795e3a62543e207d130642fcc146
Kelly Moreira <moreira.kd@outlook.com>
2019-10-11 12:50:24 -0400

    seeds ran

--- db/seeds/01-projects.js
+++ db/seeds/01-projects.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        {name: 'coffeefable', description: 'an app where short story writers can share their work while earning', completed: false},
+        {name: 'marathon training', completed: false},
+        {name: 'yoga nonprofit', description: 'donation based yoga where money goes towards SF homeless', completed: false}
+       
       ]);
     });
 };
