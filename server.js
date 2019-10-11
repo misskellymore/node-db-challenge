a97d9529e1345a47a65eea7cc271a16507e9f7ed
Kelly Moreira <moreira.kd@outlook.com>
2019-10-11 11:20:32 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const route = require('./routes/routes.js');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/work', route);
+
+module.exports = server;
