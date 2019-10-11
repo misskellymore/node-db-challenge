013e21c79258935fe9a2c44143d37611723b546b
Kelly Moreira <moreira.kd@outlook.com>
2019-10-11 12:04:44 -0400

    created tbls and drop tbls

--- db/migrations/20191011111220_bootstrap.js
+++ db/migrations/20191011111220_bootstrap.js
@@ -1,8 +1,82 @@
 
 exports.up = function(knex) {
+
+    return knex.schema.createTable('projects', tbl => {
+
+        tbl.increments();
+        tbl.string('name').notNullable();
+        tbl.text('description');
+        tbl.boolean('completed').defaultTo(false).notNullable();
+
+    })
+
+    .createTable('resources', tbl => {
+
+        tbl.increments();
+        tbl.string('name').unique().notNullable();
+        tbl.text('description');
+
+    })
+
+    .createTable('tasks', tbl => {
+
+        tbl.increments();        
+        tbl.text('description').notNullable();
+        tbl.text('notes');
+        tbl.boolean('completed').defaultTo('false').notNullable();
+
+
+        // foreign key to projects
+
+        tbl.integer('project_id')
+        // forces integer to be positive
+        .unsigned()
+        .notNullable()
+        .references('id')
+        // this table must exist already
+        .inTable('projects')
+
+        
+
+    })
+
+    .createTable('project_resources', tbl => {
+
+        // foreign key to projects
+        
+        tbl.integer('project_id')
+        // forces integer to be positive
+        .unsigned()
+        .notNullable()
+        .references('id')
+        // this table must exist already
+        .inTable('recipes')
+
+
+
+        // foreign key to resources
+
+        tbl.integer('resource_id')
+        // forces integer to be positive
+        .unsigned()
+        .notNullable()
+        .references('id')
+        // this table must exist already
+        .inTable('resources')
+    })
+
+    
   
 };
 
+
+
+
 exports.down = function(knex) {
+
+    return knex.schema.dropTableIfExists('project_resources')
+                      .dropTableIfExists('tasks')
+                      .dropTableIfExists('resources')
+                      .dropTableIfExists('projects')
   
 };
