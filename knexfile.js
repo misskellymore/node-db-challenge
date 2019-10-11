6b640638c073e2f16395ce143a0c64e65e677802
Kelly Moreira <moreira.kd@outlook.com>
2019-10-11 11:10:47 -0400

    configured pkg

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,28 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './db/node'
+    },
+
+    useNullAsDefault: true,
+
+    migrations: {
+      directory: './db/migrations'
+    },
+
+    seeds: {
+      directory: './db/seeds'
     }
+
+
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+  pool: {
+    afterCreate: (conn, done) => {
+      // runs after a connection is made to the sqlite engine
+      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
     }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
 
 };
